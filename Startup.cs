a508961abaa2f74d43d554e39105d5f1130fc4a0
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2017-08-04 22:50:13 +0200

    Created initial database, applied constraints, seeded database and Makes Controller added.

--- Startup.cs
+++ Startup.cs
@@ -30,7 +30,7 @@ namespace Vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<VegaDbContext>(options=>options.UseSqlServer(Configuration["ConnectionStrings:Work"]));
+            services.AddDbContext<VegaDbContext>(options=>options.UseSqlServer(Configuration["ConnectionStrings:Home"]));
 
             // Add framework services.
             services.AddMvc();
