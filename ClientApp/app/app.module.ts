18e98635c4e3d4afe4d7a34553d95ec76a4493b5
Blagojce Jovanoski <blagojcejovanoski@gmail.com>
2017-08-18 15:09:23 +0200

    Logging errors.

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,3 +1,4 @@
+import * as Raven from 'raven-js';
 import { AppErrorHandler } from './app.error-handler';
 import { ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
@@ -12,6 +13,8 @@ import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 import {ToastyModule} from 'ng2-toasty';
+
+Raven.config('https://dcdd483a1fbc43d78a9c3fcfded24cb4@sentry.io/205526').install();
 
 @NgModule({
     bootstrap: [ AppComponent ],
